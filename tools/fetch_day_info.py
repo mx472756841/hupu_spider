bfa673de9ac4f7d85060cb0eeebc15f493c6fac4
mengx <mengx@funsun.cn>
2020-05-10 22:29:00 +0800

    增加处理。历史不存在日榜的文章和评论

--- tools/fetch_day_info.py
+++ tools/fetch_day_info.py
@@ -1,7 +1,7 @@
 import json
 
 import pymysql
-
+import datetime
 from settings import logger
 
 
@@ -12,6 +12,7 @@ def main():
     """
     try:
         logger.info("处理历史文章开始")
+        print(f"{datetime.datetime.now()} 处理历史文章开始")
         connection = pymysql.connect(host='115.159.119.204',
                                      user='root',
                                      password='BnakQkfF2sf1',
@@ -48,13 +49,15 @@ def main():
                     logger.exception(f"处理失败 {article}")
         connection.commit()
         logger.info("处理历史文章结束")
+        print(f"{datetime.datetime.now()} 处理历史文章结束")
     except:
         logger.exception("处理历史文章失败")
 
     try:
         logger.info("处理历史评论开始")
-
-        for i in range(0, 1212016, 20000):
+        print(f"{datetime.datetime.now()} 处理历史评论开始")
+        for i in range(0, 1212016, 5000):
+            print(f"{datetime.datetime.now()} 开始处理第{(i + 1) * 5000}条评论")
             connection = pymysql.connect(host='115.159.119.204',
                                          user='root',
                                          password='BnakQkfF2sf1',
@@ -70,7 +73,7 @@ def main():
                         persons 
                     FROM
                         hupu_comment 
-                    WHERE id > {} limit 20000
+                    WHERE id > {} limit 5000
                 """.format(i)
                 cursor.execute(sql)
                 comments = cursor.fetchall()
@@ -87,5 +90,9 @@ def main():
                     except:
                         logger.exception(f"处理失败 {comment}")
             connection.commit()
+
+            print(f"{datetime.datetime.now()} 处理完成第{(i + 1) * 5000}条评论")
+
+        print(f"{datetime.datetime.now()} 处理历史评论结束")
     except:
         logger.exception("处理历史评论失败")
