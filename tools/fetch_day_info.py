94a082b7a38c4834d1a54b1646dfb88231bdf6fd
mengx <mengx@funsun.cn>
2020-05-10 22:56:12 +0800

    增加处理。历史不存在日榜的文章和评论

--- tools/fetch_day_info.py
+++ tools/fetch_day_info.py
@@ -10,54 +10,54 @@ def main():
     处理历史数据
     :return:
     """
-    try:
-        logger.info("处理历史文章开始")
-        print(f"{datetime.datetime.now()} 处理历史文章开始")
-        connection = pymysql.connect(host='115.159.119.204',
-                                     user='root',
-                                     password='BnakQkfF2sf1',
-                                     db='hupu',
-                                     port=10020,
-                                     charset='utf8mb4',
-                                     cursorclass=pymysql.cursors.DictCursor)
-
-        with connection.cursor() as cursor:
-            # 处理文章
-            sql = """
-                SELECT
-                    DATE_FORMAT( publish_date, '%Y-%m-%d' ) as datestr,
-                    persons 
-                FROM
-                    hupu_article 
-                WHERE
-                    id BETWEEN 33394052 
-                    AND 35170245
-            """
-            cursor.execute(sql)
-            articles = cursor.fetchall()
-            for article in articles:
-                try:
-                    persons = json.loads(article['persons'])
-                    for person in persons:
-                        # 插入周榜信息
-                        sql = """
-                            INSERT INTO hupu_day_list(`day`, person_id, article_cnt)
-                            VALUE(%s, %s, 1) ON DUPLICATE KEY UPDATE article_cnt = article_cnt + 1
-                        """
-                        cursor.execute(sql, [article['datestr'], person])
-                except:
-                    logger.exception(f"处理失败 {article}")
-        connection.commit()
-        logger.info("处理历史文章结束")
-        print(f"{datetime.datetime.now()} 处理历史文章结束")
-    except:
-        logger.exception("处理历史文章失败")
+    # try:
+    #     logger.info("处理历史文章开始")
+    #     print(f"{datetime.datetime.now()} 处理历史文章开始")
+    #     connection = pymysql.connect(host='115.159.119.204',
+    #                                  user='root',
+    #                                  password='BnakQkfF2sf1',
+    #                                  db='hupu',
+    #                                  port=10020,
+    #                                  charset='utf8mb4',
+    #                                  cursorclass=pymysql.cursors.DictCursor)
+    #
+    #     with connection.cursor() as cursor:
+    #         # 处理文章
+    #         sql = """
+    #             SELECT
+    #                 DATE_FORMAT( publish_date, '%Y-%m-%d' ) as datestr,
+    #                 persons
+    #             FROM
+    #                 hupu_article
+    #             WHERE
+    #                 id BETWEEN 33394052
+    #                 AND 35170245
+    #         """
+    #         cursor.execute(sql)
+    #         articles = cursor.fetchall()
+    #         for article in articles:
+    #             try:
+    #                 persons = json.loads(article['persons'])
+    #                 for person in persons:
+    #                     # 插入周榜信息
+    #                     sql = """
+    #                         INSERT INTO hupu_day_list(`day`, person_id, article_cnt)
+    #                         VALUE(%s, %s, 1) ON DUPLICATE KEY UPDATE article_cnt = article_cnt + 1
+    #                     """
+    #                     cursor.execute(sql, [article['datestr'], person])
+    #             except:
+    #                 logger.exception(f"处理失败 {article}")
+    #     connection.commit()
+    #     logger.info("处理历史文章结束")
+    #     print(f"{datetime.datetime.now()} 处理历史文章结束")
+    # except:
+    #     logger.exception("处理历史文章失败")
 
     try:
         logger.info("处理历史评论开始")
         print(f"{datetime.datetime.now()} 处理历史评论开始")
-        for i in range(0, 1212016, 5000):
-            print(f"{datetime.datetime.now()} 开始处理第{(i + 1) * 5000}条评论")
+        for i in range(5000, 1212016, 5000):
+            print(f"{datetime.datetime.now()} 开始处理第{(i/5000 + 1) * 5000}条评论")
             connection = pymysql.connect(host='115.159.119.204',
                                          user='root',
                                          password='BnakQkfF2sf1',
@@ -77,6 +77,7 @@ def main():
                 """.format(i)
                 cursor.execute(sql)
                 comments = cursor.fetchall()
+                print(f"{datetime.datetime.now()} 本次共处理第{len(comments)}条评论")
                 for comment in comments:
                     try:
                         persons = json.loads(comment['persons'])
@@ -91,7 +92,7 @@ def main():
                         logger.exception(f"处理失败 {comment}")
             connection.commit()
 
-            print(f"{datetime.datetime.now()} 处理完成第{(i + 1) * 5000}条评论")
+            print(f"{datetime.datetime.now()} 处理完成第{(i/5000 + 1) * 5000}条评论")
 
         print(f"{datetime.datetime.now()} 处理历史评论结束")
     except:
